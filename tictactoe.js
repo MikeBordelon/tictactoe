f8383ac7717aaab96232ae6b9177198389df7921
Mike Bordelon <374mike@gmail.com>
2016-10-18 19:26:44 -0700

    The game works, but has many bugs. Runs in Node now

--- tictactoe.js
+++ tictactoe.js
@@ -1,43 +1,55 @@
+///////////////BROKEN
+
+var prompt = require('prompt-sync')();
+
 var playGame = function() {
 
 var checkBoardX = function() {
   if (board[0][0] === 'X' && board[0][1] === 'X' && board[0][2] === 'X') {
     hasWon = true;
+    console.log('You Won!');
     return 'You Won';
   }
 
   if (board[0][0] === 'X' && board[1][0] === 'X' && board[2][0] === 'X') {
     hasWon = true;
+    console.log('You Won!');
     return 'You Won';
   }
 
   if (board[0][0] === 'X' && board[1][1] === 'X' && board[2][2] === 'X') {
     hasWon = true;
+    console.log('You Won!');
     return 'You Won';
   }
 
   if (board[0][1] === 'X' && board[1][1] === 'X' && board[2][1] === 'X') {
     hasWon = true;
+    console.log('You Won!');
     return 'You Won';
   }
 
   if (board[0][2] === 'X' && board[1][2] === 'X' && board[2][2] === 'X') {
     hasWon = true;
+    console.log('You Won!');
     return 'You Won';
   }
 
   if (board[0][2] === 'X' && board[1][1] === 'X' && board[2][0] === 'X') {
     hasWon = true;
+    console.log('You Won!');
     return 'You Won';
   }
 
   if (board[1][0] === 'X' && board[1][1] === 'X' && board[1][2] === 'X') {
     hasWon = true;
+    console.log('You Won!');
     return 'You Won';
   }
 
   if (board[2][0] === 'X' && board[2][1] === 'X' && board[2][2] === 'X') {
     hasWon = true;
+    console.log('You Won!');
     return 'You Won';
   }
 
@@ -46,41 +58,49 @@ var checkBoardX = function() {
 var checkBoardO = function() {
   if (board[0][0] === 'O' && board[0][1] === 'O' && board[0][2] === 'O') {
     hasWon = true;
+    console.log('You Won!');
     return 'You Won';
   }
 
   if (board[0][0] === 'O' && board[1][0] === 'O' && board[2][0] === 'O') {
     hasWon = true;
+    console.log('You Won!');
     return 'You Won';
   }
 
   if (board[0][0] === 'O' && board[1][1] === 'O' && board[2][2] === 'O') {
     hasWon = true;
+    console.log('You Won!');
     return 'You Won';
   }
 
   if (board[0][1] === 'O' && board[1][1] === 'O' && board[2][1] === 'O') {
     hasWon = true;
+    console.log('You Won!');
     return 'You Won';
   }
 
   if (board[0][2] === 'O' && board[1][2] === 'O' && board[2][2] === 'O') {
     hasWon = true;
+    console.log('You Won!');
     return 'You Won';
   }
 
   if (board[0][2] === 'X' && board[1][1] === 'X' && board[2][0] === 'X') {
     hasWon = true;
+    console.log('You Won!');
     return 'You Won';
   }
 
   if (board[1][0] === 'X' && board[1][1] === 'X' && board[1][2] === 'X') {
     hasWon = true;
+    console.log('You Won!');
     return 'You Won';
   }
 
   if (board[2][0] === 'X' && board[2][1] === 'X' && board[2][2] === 'X') {
     hasWon = true;
+    console.log('You Won!');
     return 'You Won';
   }
 
@@ -97,31 +117,38 @@ var checkBoardO = function() {
   ['', '', '']
 ];
 
-var addX = function(x, y) {
-  board[x][y] = 'X';
-};
+  var addX = function(x, y) {
+    board[x][y] = 'X';
+  };
 
-var addO = function(x, y) {
-  board[x][y] = 'O';
-};
+  var addO = function(x, y) {
+    board[x][y] = 'O';
+  };
 
-  var count = 0;
+  var count = -1;
   var hasWon = false;
 
   while (!hasWon) {
+    count++;
     checkBoardO();
     checkBoardX();
 
     if (count % 2 === 0) {
-      addX(prompt('Player1 Go!, Enter your XY coords like: 0, 0'));
-      count++;
+      var xCoord = prompt('Player1 Go!, Enter your X coord');
+      var yCoord = prompt('Player1 Go!, Enter your Y coord');
+      console.log(xCoord);
+      console.log(yCoord);
 
+      addX(xCoord, yCoord);
+      console.log(board);
     }
 
     if (count % 2 !== 0) {
-      addO(prompt('Player1 Go!, Enter your XY coords like: 0, 0'));
-      count++;
+      var xCoord = prompt('Player2 Go!, Enter your X coord');
+      var yCoord = prompt('Player2 Go!, Enter your Y coord');
 
+      addO(xCoord, yCoord);
+      console.log(board);
     }
   }
 
@@ -130,4 +157,4 @@ var addO = function(x, y) {
 
 playGame();
 
-console.log(board);
+// console.log(board);
